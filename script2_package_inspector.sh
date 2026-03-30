4042cd0d695d9f59690f0e1bdd87743a418b18a7
Anushka-9211 <18anushkas@gmail.com>
2026-03-30 10:58:59 +0530

    Create script2_package_inspector.sh

---
+++ script2_package_inspector.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Script 2: FOSS Package Inspector
+# By: Anushka Singh
+
+PACKAGE="python"
+
+if command -v python >/dev/null 2>&1; then
+ echo "$PACKAGE is installed."
+ python --version
+else
+ echo "$PACKAGE is not installed."
+fi
+
+echo "Python: community-driven language used worldwide"
