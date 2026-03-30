24b37bdbb1013bfd801fcb1185bd974344b7e445
Anushka-9211 <18anushkas@gmail.com>
2026-03-30 10:59:38 +0530

    Create script3_disk_auditor.sh

---
+++ script3_disk_auditor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script 3: Disk and Permission Auditor
+# By: Anushka Singh
+
+DIRS=("/" "/home" "/tmp")
+
+echo "Directory Audit Report"
+
+for DIR in "${DIRS[@]}"; do
+ if [ -d "$DIR" ]; then
+ PERMS=$(ls -ld $DIR 2>/dev/null | awk '{print $1, $3, $4}')
+ SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
+ echo "$DIR => Permissions: $PERMS | Size: $SIZE"
+ else
+ echo "$DIR not found"
+ fi
+done
